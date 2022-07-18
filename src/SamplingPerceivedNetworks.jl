1c4a1485260083374adff90a540efc81d80a3fc7
Eric Martin Feltham <eric.feltham@yale.edu>
2022-07-18 17:04:18 -0400

    remove plotting functions

--- src/SamplingPerceivedNetworks.jl
+++ src/SamplingPerceivedNetworks.jl
@@ -3,20 +3,20 @@
 module SamplingPerceivedNetworks
 
     using Graphs, MetaGraphs, Random
-    using CairoMakie, GraphMakie, NetworkLayout, Colors, ColorSchemes
+    # using CairoMakie, GraphMakie, NetworkLayout, Colors, ColorSchemes
     import StatsBase.sample
 
     include("orbit.jl")
-    include("plotting.jl")
-    include("context.jl")
+    # include("plotting.jl")
+    # include("context.jl")
     include("sampling.jl")
     include("whole_network.jl")
     include("utilities.jl")
 
     export
         vertexorbit, vertexorbit_d,
-        context_graph,
-        plot_orbit_d, plot_context,
+        # context_graph,
+        # plot_orbit_d, plot_context,
         samplingbins, samplebins, samplenetwork,
         sort_edgelist!, psn_edgelist, psn_edgelists, edgelist
 end
