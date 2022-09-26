8a4e136cd5431e52cdf9fd98e7c509f3214b27b1
Eric Martin Feltham <eric.feltham@yale.edu>
2022-09-26 14:10:22 -0400

    reorganization

---
+++ src/_samplenet!.jl
@@ -0,0 +1,99 @@
+# _samplenet!.jl
+
+function _samplenet!(
+    seeds,
+    verticeslists,
+    graph,
+    consents,
+    dₘₐₓ,
+    desired,
+    dvals,
+    moreinfo,
+    shuffle
+)
+
+    for (v, seed) in zip(vertices(graph), seeds)
+
+        # if consent[node_name] == 0 then skip, since
+        # the person will not be surveyed for css
+        nodename = get_prop(graph, v, :name)
+
+        # consent states:         
+        # 0: not consented to either
+        # 1: consented to survey
+        # 2: consented to survey and photo
+        # 3: consented to photo, but not survey
+        # include as perceivers those who agreed to survey
+        if (consents[nodename] == 1) | (consents[nodename] == 2)
+
+            rad = vertexorbit(graph, v, dₘₐₓ);
+            bins = samplingbins(rad, dₘₐₓ, consents);
+        
+            # only one call per seed number (i.e., one seed per villager)
+            verticeslists[nodename] = samplebins(
+                MersenneTwister(seed),
+                bins;
+                desired = desired, dvals = dvals, moreinfo = moreinfo
+            )
+
+            if shuffle
+                # length of the list of pairs
+                # drop [1] index since we don't keep
+                # (pairs, degree, realness) -> only pairs
+                ln = length(verticeslists[nodename]);
+                if ln > 1 # if there is more than one valid pair
+                    # shuffle the pairs for random presentation
+                    idx = sample(
+                        MersenneTwister(seed), collect(1:ln), ln;
+                        replace = false
+                    )
+                    # iterate over the info types (pairs, degree, realness)
+                    # we don't store all of these for branch "Server"
+                    # so don't iterate
+                    if !moreinfo
+                        permute!(verticeslists[nodename], idx)
+                    else
+                        for k in 1:3
+                            permute!(verticeslists[nodename][k], idx)
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+function _samplenet!(
+    seeds,
+    verticeslists, graph, dₘₐₓ, desired, dvals, moreinfo, shuffle
+)
+
+    for (v, seed) in zip(vertices(graph), seeds)
+        rad = vertexorbit(graph, v, dₘₐₓ); # 2.79 MiB
+        bins = samplingbins(rad, dₘₐₓ);
+    
+        verticeslists[get_prop(graph, v, :name)] = samplebins(
+            MersenneTwister(seed),
+            bins;
+            desired = desired, dvals = dvals, moreinfo = moreinfo
+        )
+
+        if shuffle
+            # length of the list of pairs
+            # drop [1] index since we don't keep
+            # (pairs, degree, realness) -> only pairs
+            ln = length(verticeslists[get_prop(graph, v, :name)]);
+            if ln > 1 # if there is more than one valid pair
+                # shuffle the pairs for random presentation
+                idx = sample(
+                    MersenneTwister(seed), collect(1:ln), ln;
+                    replace = false
+                )
+                # iterate over the info types (pairs, degree, realness)
+                # we don't store all of these for branch "Server"
+                # so don't iterate
+                permute!(verticeslists[get_prop(graph, v, :name)], idx)
+            end
+        end
+    end
+end
--- src/samplenetwork.jl
+++ src/samplenetwork.jl
@@ -1,49 +1,15 @@
 # whole_network.jl
-
 # sampling lists for the whole network
 
-"""
-        samplenetwork(
-            [rng],
-            graph;
-            desired = ((10, 10), (5, 5), (5, 5)), dvals = (1:2, 3, 4),
-            moreinfo = false,
-            shuffle = true
-        )
+VerticesList1 = Dict{String, Vector{Tuple{String, String}}};
+VerticesList2 = Dict{String, Tuple{Vector{Tuple{String, String}}, Vector{Union{Int, UnitRange}}, Vector{Bool}}};
 
-Given an unweighted and undirected sociocentric network, generate
-a list of relationships to show each person in the network, based on the
-"social orbit" procedure. Sample a number at each degree, for real vs.
-counterfactual, according to the amounts specified by desired for each degree
-given by `dvals`.
-
-`desired` and `dvals` are in the same order. `dvals` contains either
-integers, or integer ranges (for the combination of degrees into one bin).
-`desired` is a list of tuples, where each element corresponds to the
-degree bin specified by `dvals`, at that index.
-The format is (# real, # counterfactual)).
-
-Optionally specify a random number generator (rng).
-"""
-function samplenetwork(
-    graph;
-    desired = ((10, 10), (5, 5), (5, 5)), dvals = (1:2, 3, 4),
-    moreinfo = false,
-    shuffle = true
-)    
-
-    dₘₐₓ = maximum(reduce(vcat, dvals))
-
-    verticeslists = if !moreinfo
-        Dict{String, Vector{Tuple{String, String}}}()
+function verticeslist(moreinfo)
+    return if !moreinfo
+        VerticesList1()
     else
-        Dict{String, Tuple{Vector{Tuple{String, String}}, Vector{Union{Int, UnitRange}}, Vector{Bool}}}()
+        VerticesList2()
     end
-    
-    sizehint!(verticeslists, nv(graph))
-    _samplenet!(verticeslists, graph, dₘₐₓ, desired, dvals, moreinfo, shuffle)
-    
-    return verticeslists
 end
 
 """
@@ -78,11 +44,10 @@ function samplenetwork(
 
     dₘₐₓ = maximum(reduce(vcat, dvals))
 
-    verticeslists = if !moreinfo
-        Dict{String, Vector{Tuple{String, String}}}()
-    else
-        Dict{String, Tuple{Vector{Tuple{String, String}}, Vector{Union{Int, UnitRange}}, Vector{Bool}}}()
-    end
+    verticeslists = verticeslist(moreinfo)
+
+    # alternative to vertices lists
+    # edgetab = init_edgetable(nv(graph) * countdesired(desired), moreinfo);
     
     # create a seed number for each perceiver
     # using village seed (input exogenously)
@@ -92,7 +57,9 @@ function samplenetwork(
     );
 
     sizehint!(verticeslists, nv(graph))
-    _samplenet!(seeds, verticeslists, graph, dₘₐₓ, desired, dvals, moreinfo, shuffle)
+    _samplenet!(
+        seeds, verticeslists, graph, dₘₐₓ, desired, dvals, moreinfo, shuffle
+    )
     
     return verticeslists
 end
@@ -121,41 +88,6 @@ function _samplenet!(
                 for i in eachindex(verticeslists[get_prop(graph, v, :name)])
                     permute!(verticeslists[get_prop(graph, v, :name)][i], idx)
                 end
-            end
-        end
-    end
-end
-
-function _samplenet!(
-    seeds,
-    verticeslists, graph, dₘₐₓ, desired, dvals, moreinfo, shuffle
-)
-    # (v, seed) = collect(zip(vertices(graph), seeds))[1]
-    for (v, seed) in zip(vertices(graph), seeds)
-        rad = vertexorbit(graph, v, dₘₐₓ); # 2.79 MiB
-        bins = samplingbins(rad, dₘₐₓ);
-    
-        verticeslists[get_prop(graph, v, :name)] = samplebins(
-            MersenneTwister(seed),
-            bins;
-            desired = desired, dvals = dvals, moreinfo = moreinfo
-        )
-
-        if shuffle
-            # length of the list of pairs
-            # drop [1] index since we don't keep
-            # (pairs, degree, realness) -> only pairs
-            ln = length(verticeslists[get_prop(graph, v, :name)]);
-            if ln > 1 # if there is more than one valid pair
-                # shuffle the pairs for random presentation
-                idx = sample(
-                    MersenneTwister(seed), collect(1:ln), ln;
-                    replace = false
-                )
-                # iterate over the info types (pairs, degree, realness)
-                # we don't store all of these for branch "Server"
-                # so don't iterate
-                permute!(verticeslists[get_prop(graph, v, :name)], idx)
             end
         end
     end
@@ -198,12 +130,11 @@ function samplenetwork(
 
     dₘₐₓ = maximum(reduce(vcat, dvals))
 
-    verticeslists = if !moreinfo
-        Dict{String, Vector{Tuple{String, String}}}()
-    else
-        Dict{String, Tuple{Vector{Tuple{String, String}}, Vector{Union{Int, UnitRange}}, Vector{Bool}}}()
-    end
-    
+    verticeslists = verticeslist(moreinfo)
+
+    # alternative to vertices lists
+    # edgetab = init_edgetable(nv(graph) * countdesired(desired), moreinfo);
+
     # create a seed number for each perceiver
     # using village seed (input exogenously)
     seeds = sample(
@@ -220,64 +151,19 @@ function samplenetwork(
     return verticeslists
 end
 
-function _samplenet!(
-    seeds,
-    verticeslists,
-    graph,
-    consents,
-    dₘₐₓ,
-    desired,
-    dvals,
-    moreinfo,
-    shuffle
-)
-    for (v, seed) in zip(vertices(graph), seeds)
+function samplenetwork(
+    graph;
+    desired = ((10, 10), (5, 5), (5, 5)), dvals = (1:2, 3, 4),
+    moreinfo = false,
+    shuffle = true
+)    
 
-        # if consent[node_name] == 0 then skip, since
-        # the person will not be surveyed for css
-        nodename = get_prop(graph, v, :name)
+    dₘₐₓ = maximum(reduce(vcat, dvals))
 
-        # consent states:         
-        # 0: not consented to either
-        # 1: consented to survey
-        # 2: consented to survey and photo
-        # 3: consented to photo, but not survey
-        # include as perceivers those who agreed to survey
-        if (consents[nodename] == 1) | (consents[nodename] == 2)
-
-            rad = vertexorbit(graph, v, dₘₐₓ);
-            bins = samplingbins(rad, dₘₐₓ, consents);
-        
-            # only one call per seed number (i.e., one seed per villager)
-            verticeslists[nodename] = samplebins(
-                MersenneTwister(seed),
-                bins;
-                desired = desired, dvals = dvals, moreinfo = moreinfo
-            )
-
-            if shuffle
-                # length of the list of pairs
-                # drop [1] index since we don't keep
-                # (pairs, degree, realness) -> only pairs
-                ln = length(verticeslists[nodename]);
-                if ln > 1 # if there is more than one valid pair
-                    # shuffle the pairs for random presentation
-                    idx = sample(
-                        MersenneTwister(seed), collect(1:ln), ln;
-                        replace = false
-                    )
-                    # iterate over the info types (pairs, degree, realness)
-                    # we don't store all of these for branch "Server"
-                    # so don't iterate
-                    if !moreinfo
-                        permute!(verticeslists[nodename], idx)
-                    else
-                        for k in 1:3
-                            permute!(verticeslists[nodename][k], idx)
-                        end
-                    end
-                end
-            end
-        end
-    end
+    verticeslists = verticeslist(moreinfo)
+    
+    sizehint!(verticeslists, nv(graph))
+    _samplenet!(verticeslists, graph, dₘₐₓ, desired, dvals, moreinfo, shuffle)
+    
+    return verticeslists
 end
