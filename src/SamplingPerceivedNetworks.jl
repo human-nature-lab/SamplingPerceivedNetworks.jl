dd7256e5d5eabb2b5439f1278741428a253d4f70
Eric Martin Feltham <eric.feltham@yale.edu>
2022-10-04 16:13:17 -0400

    remove irrelevant code

--- src/SamplingPerceivedNetworks.jl
+++ src/SamplingPerceivedNetworks.jl
@@ -3,12 +3,9 @@
 module SamplingPerceivedNetworks
 
     using Graphs, MetaGraphs, Random
-    # using CairoMakie, GraphMakie, NetworkLayout, Colors, ColorSchemes
     import StatsBase.sample
 
     include("orbit.jl")
-    # include("plotting.jl")
-    # include("context.jl")
     include("samplingbins.jl")
     include("_samplenet!.jl")
     include("samplebins.jl")
@@ -18,8 +15,6 @@ module SamplingPerceivedNetworks
 
     export
         vertexorbit, vertexorbit_d,
-        # context_graph,
-        # plot_orbit_d, plot_context,
         samplingbins, samplebins, samplenetwork,
         sort_edgelist!, psn_edgelist, psn_edgelists, edgelist, countdesired,
         tuplesort!
